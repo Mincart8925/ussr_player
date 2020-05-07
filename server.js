b238355a7f59c2a574ba3e862a879fb2d098828e
마카 <55223245+Mincart8925@users.noreply.github.com>
2020-05-07 22:48:38 +0900

    읽기 편하게 약간의 엔터질

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 let app = express();
 
 app.use('/res', express.static('res'));
+
 app.get('/',(req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
 app.listen(80,()=>console.log('ready!'));
