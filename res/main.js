f1b2865cf2a6b9aee653379420fb88eb3cd51715
Wiki Updater <minecart8926@daum.net>
2020-05-07 17:32:43 +0900

    대애충 소리조절 커밋

--- res/main.js
+++ res/main.js
@@ -1,8 +1,17 @@
+var x = null
 function play_ussr(){
-    var x = document.querySelector('audio');
+    x = document.querySelector('audio');
     x.volume = 0.5;
     x.play();
     x.addEventListener('ended', () => {
         x.play();
     });
 }
+
+document.addEventListener('keydown', (e)=>{
+    if(e.key == "ArrowUp"){
+        x.volume += 0.1;
+    } else if(e.key == "ArrowDown"){
+        x.volume -= 0.1
+    }
+});
